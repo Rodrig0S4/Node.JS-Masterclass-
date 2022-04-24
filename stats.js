9441adafc904cc1fd982f4d30a8729adfe30da50
Razzkz <sdearaujo.rodrigo@gmail.com>
2022-04-24 13:17:41 -0300

    end stats

--- stats.js
+++ stats.js
@@ -1,17 +1,25 @@
 const os = require('os')
 
-const {freemem, totalmem} = os
+setInterval(() => {
 
-const total = parseInt(totalmem() / 1024 / 1024)
-const mem = parseInt(freemem() / 1024 / 1024)
-const percents = parseInt((mem / total) * 100)
+    const { freemem, totalmem } = os
 
-const stats = {
-    free: `${mem} MB`,
-    total: `${total} MB`,
-    usage:`${percents} %`
-}
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const mem = parseInt(freemem() / 1024 / 1024)
+    const percents = parseInt((mem / total) * 100)
 
-console.table(stats)
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents} %`
+    }
+
+    console.clear()
+    console.log("=== PC STATS ===")
+    console.table(stats)
+
+}, 1000)
+
+
 
 //console.log(`${parseInt(freemem() / 1024 / 1024)} MB` , totalmem())
